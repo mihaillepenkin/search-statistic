e4746e110e0f679b4bf0c5a6a4897bc36261e1dd
Misha <mihaillepenkin@yandex.ru>
2026-05-25 17:04:43 +0300

    fix playload

--- internal/domain/entity.go
+++ internal/domain/entity.go
@@ -3,9 +3,6 @@ package domain
 type Message struct {
 	Query        string `json:"query"`
 	TimestampSec int64  `json:"timestamp_sec"`
-	UserID       string `json:"user_id,omitempty"`
-	IP           string `json:"ip,omitempty"`
-	SessionID    string `json:"session_id,omitempty"`
 }
 
 type QueryCount struct {
